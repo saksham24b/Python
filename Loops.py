9352a5c65bfb358bd9f597ada13ded63f73055c6
Saksham <145268917+saksham24b@users.noreply.github.com>
2024-03-30 13:10:50 +0000

    test

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# Loops
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    n = int(input("Enter the no of echo: "))
+    if n > 0:
+        return n
+    
+def meow(num):
+    for _ in range(num):
+        print("meow")
+
