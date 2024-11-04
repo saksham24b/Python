e176267cfe26a405244d613587b7e03657ec56a2
Saksham <saksham24b@gmail.com>
2024-11-04 17:56:33 +0530

    Extract username from url

--- username.py
+++ username.py
@@ -1,5 +1,5 @@
 import re
 
 url = input("URL: ").strip()
-if matches := re.search(r".[com|net|in|org]/([^/]+)", url):
+if matches := re.search(r".[com|net|in|org]/([a-z0-9_]+)", url):
     print(f"Twitter username: @{matches.group(1)}")
